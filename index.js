5499a17ac05e25bd1ad2a8bd5b40d2899932e800
jw20191n <clairejw1015@gmail.com>
2019-10-14 15:43:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+//map methods
+function mapToNegativize(array){
+    let newArray= [];
+    for(let item of array){
+        item = item * -1;
+        newArray.push(item);
+    }
+    return newArray;
+}
+
+function mapToNoChange(array){
+    return array;
+}
+
+function mapToDouble(array){
+    let newArray= [];
+    for(let item of array){
+        item = item * 2;
+        newArray.push(item);
+    }
+    return newArray;
+}
+
+function mapToSquare(array){
+    let newArray= [];
+    for(let item of array){
+        item = item * item;
+        newArray.push(item);
+    }
+    return newArray;
+}
+
+//reduce-like methods
+function reduceToTotal(array, startingPoint=0){
+    let sum = startingPoint;
+    console.log(startingPoint);
+    for(let i=0; i<array.length; i++){
+        sum += array[i];
+    }
+    return sum;  
+}
+
+function reduceToAllTrue(array){
+    let result = true;
+    for(let i=0; i<array.length; i++){
+        if (!array[i]){
+            return false;
+        }
+    }
+    return result;
+}
+
+function reduceToAnyTrue(array){
+    let result = false;
+    for (let i=0; i<array.length; i++){
+        if (!!array[i]){
+            return true;
+        }
+    }
+    return result;
+}
